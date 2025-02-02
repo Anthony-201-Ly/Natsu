0df43498a39f3c85c8de7f8f75bc0ca7a0256154
unknown <anthonytoanly@gmail.com>
2025-02-02 01:55:36 -0500

    Processing Wildfires

--- model-solution-2.py
+++ model-solution-2.py
@@ -1,4 +1,5 @@
 # For AI Model Training https://youtu.be/29ZQ3TDGgRQ?si=D72UOyhjeHQdENxY
+import numpy as np
 import pandas as pd
 
 # Import historical environmental data
@@ -14,28 +15,26 @@ historical_wildfire["timestamp"] = historical_wildfire["timestamp"].astype(str)
 # For the environmental data, create a new fire risk dataframe
     #  by creating a new column for fire_risk which is 0 by default
 historical_fire_risk = historical_environmental.assign(fire_risk=0)
-print(historical_fire_risk)
+#print(historical_fire_risk)
 # For every row in wildfire, if the timestamp is found in environmental data, set the fire_risk to 1
 index_environment = len(historical_environmental)
 index_wildfire = len(historical_wildfire)
 
+num_wildfire = 0
 for x in range(index_environment):
     for y in range(index_wildfire):
         timestamp_fire_risk = historical_fire_risk.at[x,"timestamp"]
         timestamp_wildfire = historical_wildfire.at[y,"timestamp"]
         if timestamp_fire_risk == timestamp_wildfire:
             historical_fire_risk.at[x,"fire_risk"] = 1
+            # Count
+            num_wildfire += 1
+            print("Found one")
             break
+print("Fire Risk Collumn Calculated")
+print(num_wildfire)
 
-#print(historical_fire_risk)
-for i in range(index_environment):
-    print(historical_fire_risk[i])
-
-
-#print(historical_environmental)
-#print(historical_wildfire)
-
-# Split data to X and y (independant and dependant)
+historical_fire_risk.to_csv("test.csv")
 
 
 
