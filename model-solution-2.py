ce86cfd506ac50a367c500d2f373272b6a018454
unknown <anthonytoanly@gmail.com>
2025-02-02 02:02:56 -0500

    Exporting Dataframe to CSV

--- model-solution-2.py
+++ model-solution-2.py
@@ -15,11 +15,10 @@ historical_wildfire["timestamp"] = historical_wildfire["timestamp"].astype(str)
 # For the environmental data, create a new fire risk dataframe
     #  by creating a new column for fire_risk which is 0 by default
 historical_fire_risk = historical_environmental.assign(fire_risk=0)
-#print(historical_fire_risk)
+
 # For every row in wildfire, if the timestamp is found in environmental data, set the fire_risk to 1
 index_environment = len(historical_environmental)
 index_wildfire = len(historical_wildfire)
-
 num_wildfire = 0
 for x in range(index_environment):
     for y in range(index_wildfire):
@@ -31,10 +30,10 @@ for x in range(index_environment):
             num_wildfire += 1
             print("Found one")
             break
-print("Fire Risk Collumn Calculated")
-print(num_wildfire)
 
-historical_fire_risk.to_csv("test.csv")
+print("Fire Risk Column Calculated")
+print("Number of matching timestamps:", num_wildfire)
+historical_fire_risk.to_csv("ProcessedData.csv")
 
 
 
