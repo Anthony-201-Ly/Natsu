12543cb02de0d087e4a548e75a7abb02a39ab943
unknown <anthonytoanly@gmail.com>
2025-02-02 03:17:34 -0500

    Switched from Random Forest Regressor to Random Forest Classifier

--- model-solution-2.py
+++ model-solution-2.py
@@ -2,8 +2,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.ensemble import RandomForestClassifier
 
 # Import historical environmental data
 historical_environmental = pd.read_csv("historical_environmental_data.csv")
@@ -51,30 +50,20 @@ print(X)
 
 ### Training the model
 # Randomly choose 20% of the dataset to be used as test data
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=100)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-# We chose Random Forest Regression model
+# We chose Random Forest Classifier model
 # due to having to having many independant variables
 # as well as having numerical values for the dependant variable (Y "whether it will be a wildfire")
-# being numerical/quantitative values rather than categorical values
-rf = RandomForestRegressor(max_depth=2, random_state=100)
+# being either 0 or 1 "Random Forest Regression gave us no values of 1 for fire_risk"
+rf = RandomForestClassifier()
 rf.fit(X_train, y_train)
 
 # Applying the model to make a prediction on the test data
 y_rf_train_pred = rf.predict(X_train)
 y_rf_test_pred = rf.predict(X_test)
 
-rf_train_mse = mean_squared_error(y_train, y_rf_train_pred)
-rf_train_r2 = r2_score(y_train, y_rf_train_pred)
-
-rf_test_mse = mean_squared_error(y_test, y_rf_test_pred)
-rf_test_r2 = r2_score(y_test, y_rf_test_pred)
-
-rf_results = pd.DataFrame(['Random forest', rf_train_mse, rf_train_r2, rf_test_mse, rf_test_r2]).transpose()
-rf_results.columns = ['Method', 'Training MSE', 'Training R2', 'Test MSE', 'Test R2']
-rf_results
-
-print(rf_results)
+#print(rf_results)
 
 ### Predict the future data
 
@@ -90,8 +79,11 @@ B = rf.predict(A)
 
 # Add fire_risk to environmental data
 future_environmental["fire_risk"] = B
+
+print(future_environmental)
+future_environmental.to_csv("Future_Predicted.csv")
 # Get only risks
 fire_risk = future_environmental[future_environmental["fire_risk"] == 1]
 
 print(fire_risk)
-# Empty DataFrame wtf?
+fire_risk.to_csv("Fire_Risks.csv")
