815b78cf7b8ff111861abb7542ed7365ed1a78a0
unknown <anthonytoanly@gmail.com>
2025-02-02 00:52:22 -0500

    Import data

--- model-solution-2.py
+++ model-solution-2.py
@@ -1,6 +1,16 @@
-import numpy as np
-import pandas as pd
-import scikit as sc
-import folium as fo
-
 # For AI Model Training https://youtu.be/29ZQ3TDGgRQ?si=D72UOyhjeHQdENxY
+import pandas as pd
+
+# Import historical environmental data
+historical_environmental = pd.read_csv("historical_environmental_data.csv")
+
+# Import historical wildfire data
+historical_wildfire = pd.read_csv("historical_wildfiredata.csv")
+
+# Compare environmental and wildfire data
+# This allows us to see whether the environmental data results in a wildfire
+
+# Split data to X and y (independant and dependant)
+
+
+
